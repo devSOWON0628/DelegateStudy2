67f3e0ab7de1281413d488c870d03f15a8e667fb
소원 박 <s2018s29@e-mirim.hs.kr>
2020-10-10 22:16:52 +0900

    초기 커밋.

---
+++ DelegateStudy2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateStudy2
+{
+    class Program
+    {
+        public delegate void SendString(string message);
+
+        static void Main(string[] args)
+        {
+            SendString sayHello, sayGoodbye, multiDelegate;
+
+            sayHello = Hello;
+            //sayHello는 Hello 메서드, 
+            //sayGoodbye 메서드는 GoodBye메서드를 주는 느낌
+            sayGoodbye = GoodBye;
+
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("박소원");
+
+            Console.WriteLine();
+
+            multiDelegate = sayGoodbye;
+            multiDelegate("박소원");
+        }
+        public static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요 " + message);
+        }
+        public static void GoodBye(string message)
+        {
+            Console.WriteLine("안녕히 가세요 " + message);
+        }
+    }
+}
